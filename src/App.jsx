092ff54656e4b4be92ec022181f4a104b9057681
christovjohn <christovarghesejohn@gmail.com>
2026-07-18 16:50:40 +0530

    Initial React Netflix UI clone

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import NavBar from './Components/NavBar/NavBar'
+import Banner from './Components/Banner/Banner'
+import Rowpost from './Components/RowPost/RowPost'
+import "./App.css"
+
+function App() {
+  return(
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <Rowpost/>
+
+    </div>
+  )
+  
+
+ 
+}
+
+export default App
