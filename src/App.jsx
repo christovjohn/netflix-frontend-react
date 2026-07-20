f63503190f47bb7c3af8dbcdbd2046781f5da1e7
christovjohn <christovarghesejohn@gmail.com>
2026-07-20 19:00:39 +0530

    added properties to RowPost

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import NavBar from './Components/NavBar/NavBar'
 import Banner from './Components/Banner/Banner'
 import Rowpost from './Components/RowPost/RowPost'
 import "./App.css"
+import { action,originals } from "./urls"
 
 function App() {
   return(
     <div className="App">
       <NavBar/>
       <Banner/>
-      <Rowpost/>
+      <Rowpost url={originals} title='Netflix Orginals'/>
+      <Rowpost url={action}title='Action' isSmall/>  
 
     </div>
   )
